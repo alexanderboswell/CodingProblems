f09d9e6d084cde48639fb0c17dc0cb2af36d2cdc
Alex Boswell <alexanderboswell@gmail.com>
2017-02-11 23:48:11 -0700

    brute force method given for ProductOfOtherNumbers problem

--- CodingProblems/ProductOfOtherNumbers/src/ProductOfOtherNumbers.java
+++ CodingProblems/ProductOfOtherNumbers/src/ProductOfOtherNumbers.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alexanderboswell on 2/11/17.
+ *
+ * interviewcake.com Question #2
+ *
+ * You have an array of integers,
+ * and for each index you want to find the product
+ * of every integer except the integer at that index.
  */
 public class ProductOfOtherNumbers {
     public static void main(String[] args) {
+        int[] inputArray = new int[]{1,7,3,4};
+        bruteForceAttempt(inputArray);
 
+    }
+    public static Object[] bruteForceAttempt(int[] inputArray){
+        List<Integer> outputList = new ArrayList<Integer>();
+        for (int i = 0; i < inputArray.length; i++) {
+            int product = 1;
+            for (int j = 0; j < inputArray.length; j++) {
+                if(inputArray[i]!=inputArray[j]){
+                    product = product * inputArray[j];
+                }
+            }
+            outputList.add(product);
+        }
+
+        return outputList.toArray();
     }
 }
